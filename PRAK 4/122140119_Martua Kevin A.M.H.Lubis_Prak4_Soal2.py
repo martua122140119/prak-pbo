ae1f62b221bf4a8c6c87c1ef1a55ff96ad881459
martua kevin <martua.122140119@student.itera.ac.id>
2024-03-31 15:47:13 +0700

    Prak4

---
+++ PRAK 4/122140119_Martua Kevin A.M.H.Lubis_Prak4_Soal2.py
@@ -0,0 +1,24 @@
+import math
+
+class BangunDatar:
+    def hitungLuas(self):
+        raise NotImplementedError
+
+class Persegi(BangunDatar):
+    def __init__(self, sisi):
+        self.sisi = sisi
+
+    def hitungLuas(self):
+        return self.sisi ** 2
+
+class Lingkaran(BangunDatar):
+    def __init__(self, jari_jari):
+        self.jari_jari = jari_jari
+
+    def hitungLuas(self):
+        return math.pi * (self.jari_jari ** 2)
+
+persegi = Persegi(5)
+lingkaran = Lingkaran(3)
+print(f"Luas Persegi: {persegi.hitungLuas()}") 
+print(f"Luas Lingkaran: {lingkaran.hitungLuas()}")
